fbb12447a84d019902f56df3ded88b36339e10ba
Willian David Calin <david2c142@gmail.com>
2023-03-18 20:06:08 -0500

    refactor: refactor openai services to improve performance

--- src/services/background/openai-services.ts
+++ src/services/background/openai-services.ts
@@ -1,5 +1,6 @@
 export const eventSourceOpenAi = (text: string) => {
-  const API_URL = "https://code-xplain-server.vercel.app/api/openai";
+  // Enter Openai Api Url
+  const API_URL = "";
 
   const url = `${API_URL}/?query=${JSON.stringify(text)}`;
 
